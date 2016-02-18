5773d4432f0ada49de2ff70c5f8d352e08839bd5
njkr <jenkins.raj@gmail.com>
2016-02-18 17:02:27 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    //objects
+
+    var s = {
+        p: "hai",
+        sam: function (push) {
+            console.log(push);
+        }
+    };
+    s.sam("retrival data ");
+
+};
